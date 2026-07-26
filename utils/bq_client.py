968c7356adeb47fe5614e42df8c0c25ee0002e9c
kautsarhasby <kautsar20204@gmail.com>
2026-07-26 20:41:47 +0700

    fix: credentials environment missing

--- utils/bq_client.py
+++ utils/bq_client.py
@@ -3,7 +3,7 @@ import os
 
 CREDENTIALS_PATH = os.getenv("GCP_CREDENTIALS", "credentials.json")
 if os.path.exists(CREDENTIALS_PATH):
-    os.environ["GOOGLE_APP_CREDENTIALS"] = CREDENTIALS_PATH
+    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH
 
 def get_bq_client():
     bq_client = bigquery.Client()
