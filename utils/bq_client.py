e585be757693a294d3750a9c9f65a357141f5614
kautsarhasby <kautsar20204@gmail.com>
2026-07-26 21:16:04 +0700

    chore: change credentials mode to service account

--- utils/bq_client.py
+++ utils/bq_client.py
@@ -1,11 +1,20 @@
 from google.cloud import bigquery
+from google.oauth2 import service_account
 import os
 
 CREDENTIALS_PATH = os.getenv("GCP_CREDENTIALS", "credentials.json")
-if os.path.exists(CREDENTIALS_PATH):
-    os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = CREDENTIALS_PATH
-
+SCOPES = [
+    "https://www.googleapis.com/auth/bigquery",
+    "https://www.googleapis.com/auth/cloud-platform"
+]
 def get_bq_client():
-    bq_client = bigquery.Client()
+    if os.path.exists(CREDENTIALS_PATH):
+        credentials = service_account.Credentials.from_service_account_file(
+            CREDENTIALS_PATH,
+            SCOPES
+        )
+        bq_client = bigquery.Client(credentials=credentials,project=credentials.project_id)
+    else:
+        bq_client = bigquery.Client()
     project_id = bq_client.project
     return bq_client, project_id
